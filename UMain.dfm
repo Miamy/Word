68d9895496e97f7861938416f3d425f4f9be89a8
miamy <miamy@ukr.net>
2015-04-01 17:21:02 +0400

    First

---
+++ UMain.dfm
@@ -0,0 +1,249 @@
+object FMain: TFMain
+  Left = 0
+  Top = 0
+  BorderStyle = bsDialog
+  Caption = 'MS Word table generator'
+  ClientHeight = 498
+  ClientWidth = 563
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  PixelsPerInch = 96
+  TextHeight = 14
+  object Label1: TLabel
+    Left = 16
+    Top = 16
+    Width = 75
+    Height = 14
+    Caption = 'Input files dir:'
+  end
+  object Label2: TLabel
+    Left = 16
+    Top = 42
+    Width = 63
+    Height = 14
+    Caption = 'Output file:'
+  end
+  object lblStatus: TLabel
+    Left = 16
+    Top = 72
+    Width = 171
+    Height = 14
+    Caption = 'Directory not processed yet'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object PictureImage: TImage
+    Left = 8
+    Top = 104
+    Width = 105
+    Height = 105
+    AutoSize = True
+    Center = True
+    Proportional = True
+    Stretch = True
+  end
+  object Label7: TLabel
+    Left = 424
+    Top = 72
+    Width = 48
+    Height = 14
+    Caption = 'Scale,%:'
+  end
+  object lblWord: TLabel
+    Left = 16
+    Top = 104
+    Width = 144
+    Height = 14
+    Caption = 'Connecting to MS Word...'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = 'Tahoma'
+    Font.Style = [fsItalic]
+    ParentFont = False
+    Visible = False
+  end
+  object edSelectPictures: TEdit
+    Left = 120
+    Top = 8
+    Width = 388
+    Height = 22
+    TabOrder = 0
+    Text = 'd:\Programs\Word\small\'
+  end
+  object edSelectDoc: TEdit
+    Left = 120
+    Top = 39
+    Width = 388
+    Height = 22
+    TabOrder = 1
+    Text = 'd:\Programs\Word\test.doc'
+  end
+  object btnSelectPictures: TButton
+    Left = 514
+    Top = 8
+    Width = 25
+    Height = 25
+    Caption = '...'
+    TabOrder = 2
+    OnClick = btnSelectPicturesClick
+  end
+  object btnSelectDoc: TButton
+    Left = 514
+    Top = 38
+    Width = 25
+    Height = 25
+    Caption = '...'
+    TabOrder = 3
+    OnClick = btnSelectDocClick
+  end
+  object btnGenerate: TButton
+    Left = 160
+    Top = 456
+    Width = 137
+    Height = 25
+    Caption = 'Generate'
+    TabOrder = 4
+    OnClick = btnGenerateClick
+  end
+  object btnExit: TButton
+    Left = 464
+    Top = 456
+    Width = 75
+    Height = 25
+    Caption = 'Exit'
+    TabOrder = 5
+    OnClick = btnExitClick
+  end
+  object btnCount: TButton
+    Left = 16
+    Top = 456
+    Width = 75
+    Height = 25
+    Caption = 'Count'
+    TabOrder = 6
+    OnClick = btnCountClick
+  end
+  object GroupBox1: TGroupBox
+    Left = 16
+    Top = 376
+    Width = 523
+    Height = 65
+    Caption = 'Margins, mm'
+    TabOrder = 7
+    object Label3: TLabel
+      Left = 12
+      Top = 27
+      Width = 26
+      Height = 14
+      Caption = 'Left:'
+    end
+    object Label4: TLabel
+      Left = 132
+      Top = 27
+      Width = 32
+      Height = 14
+      Caption = 'Right:'
+    end
+    object Label5: TLabel
+      Left = 264
+      Top = 27
+      Width = 26
+      Height = 14
+      Caption = 'Top:'
+    end
+    object Label6: TLabel
+      Left = 389
+      Top = 27
+      Width = 45
+      Height = 14
+      Caption = 'Bottom:'
+    end
+    object seLeft: TSpinEdit
+      Left = 48
+      Top = 24
+      Width = 57
+      Height = 23
+      Increment = 5
+      MaxValue = 0
+      MinValue = 0
+      TabOrder = 0
+      Value = 40
+    end
+    object seRight: TSpinEdit
+      Left = 176
+      Top = 24
+      Width = 57
+      Height = 23
+      Increment = 5
+      MaxValue = 0
+      MinValue = 0
+      TabOrder = 1
+      Value = 25
+    end
+    object seTop: TSpinEdit
+      Left = 304
+      Top = 24
+      Width = 57
+      Height = 23
+      Increment = 5
+      MaxValue = 0
+      MinValue = 0
+      TabOrder = 2
+      Value = 20
+    end
+    object seBottom: TSpinEdit
+      Left = 448
+      Top = 24
+      Width = 57
+      Height = 23
+      Increment = 5
+      MaxValue = 0
+      MinValue = 0
+      TabOrder = 3
+      Value = 20
+    end
+  end
+  object seScale: TSpinEdit
+    Left = 482
+    Top = 69
+    Width = 57
+    Height = 23
+    Increment = 10
+    MaxValue = 0
+    MinValue = 0
+    TabOrder = 8
+    Value = 100
+  end
+  object WordApplication: TWordApplication
+    AutoConnect = False
+    ConnectKind = ckNewInstance
+    AutoQuit = False
+    Left = 48
+    Top = 144
+  end
+  object WordDocument: TWordDocument
+    AutoConnect = False
+    ConnectKind = ckNewInstance
+    Left = 168
+    Top = 144
+  end
+  object SaveDialog: TSaveDialog
+    DefaultExt = '*.doc'
+    Filter = 'Word files|*.doc'
+    Left = 384
+    Top = 128
+  end
+end
---
+++ UMain.pas
@@ -0,0 +1,243 @@
+unit UMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, WordXP, Vcl.OleServer, Clipbrd,
+  Vcl.ExtCtrls, pngimage, FileCtrl, Vcl.Samples.Spin, OfficeXP, Math;
+
+type
+  TFMain = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    edSelectPictures: TEdit;
+    edSelectDoc: TEdit;
+    btnSelectPictures: TButton;
+    btnSelectDoc: TButton;
+    WordApplication: TWordApplication;
+    WordDocument: TWordDocument;
+    btnGenerate: TButton;
+    btnExit: TButton;
+    btnCount: TButton;
+    lblStatus: TLabel;
+    PictureImage: TImage;
+    SaveDialog: TSaveDialog;
+    GroupBox1: TGroupBox;
+    seLeft: TSpinEdit;
+    Label3: TLabel;
+    Label4: TLabel;
+    seRight: TSpinEdit;
+    Label5: TLabel;
+    seTop: TSpinEdit;
+    Label6: TLabel;
+    seBottom: TSpinEdit;
+    Label7: TLabel;
+    seScale: TSpinEdit;
+    lblWord: TLabel;
+    procedure btnExitClick(Sender: TObject);
+    procedure btnGenerateClick(Sender: TObject);
+    procedure btnCountClick(Sender: TObject);
+
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure btnSelectDocClick(Sender: TObject);
+    procedure btnSelectPicturesClick(Sender: TObject);
+  private
+    { Private declarations }
+    FilesList, StationsList: TStringList;
+    Counted: boolean;
+    function GetPathToPictures: string;
+
+    property PathToPictures: string read GetPathToPictures;
+    procedure CountFiles;
+
+    function CentimetersToPoints(aValue: Single): Single;
+    function MillimetersToPoints(aValue: Single): Single;
+    function PixelsToPoints(aValue: Single): Single;
+  public
+    { Public declarations }
+  end;
+
+var
+  FMain: TFMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFMain.btnCountClick(Sender: TObject);
+begin
+  CountFiles;
+end;
+
+procedure TFMain.btnExitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+function TFMain.CentimetersToPoints(aValue: Single): Single;
+begin
+  Result := 28.35 * aValue;
+end;
+
+procedure TFMain.btnGenerateClick(Sender: TObject);
+var
+  i, j, FilesPerStation: integer;
+  Shapes: InlineShapes;
+  Range: WordRange;
+begin
+  if not Counted then
+    CountFiles;
+  if StationsList.Count = 0 then
+    exit;
+  if FilesList.Count mod StationsList.Count <> 0 then
+    if MessageBox(0, 'Number of files doesn''t correspond number of station. Continue?',
+         'Confirm', MB_ICONWARNING or MB_YESNO or MB_DEFBUTTON1) = mrNo then
+      exit;
+
+  PictureImage.Visible := false;
+  lblWord.Visible := true;
+  WordApplication.Connect;
+  try
+    try
+      WordApplication.Options.CheckSpellingAsYouType := false;
+      WordApplication.Options.CheckGrammarAsYouType := false;
+
+      WordApplication.Documents.Add(EmptyParam, EmptyParam, EmptyParam, EmptyParam);
+      WordDocument.ConnectTo(WordApplication.ActiveDocument);
+      try
+        WordDocument.PageSetup.TopMargin := MillimetersToPoints(seTop.Value);
+        WordDocument.PageSetup.BottomMargin := MillimetersToPoints(seBottom.Value);
+        WordDocument.PageSetup.LeftMargin := MillimetersToPoints(seLeft.Value);
+        WordDocument.PageSetup.RightMargin := MillimetersToPoints(seRight.Value);
+
+        WordDocument.Tables.Add(WordDocument.Range, FilesList.Count + StationsList.Count, 1, EmptyParam, EmptyParam);
+
+        lblWord.Visible := false;
+        PictureImage.Visible := true;
+        FilesPerStation := FilesList.Count div StationsList.Count;
+        for i := 0 to StationsList.Count - 1 do
+        begin
+          Range := WordDocument.Tables.Item(1).Cell((i * (FilesPerStation + 1)) + 1, 1).Range;
+          Range.Text := StationsList[i];
+          Range.ParagraphFormat.Alignment := wdAlignParagraphCenter;
+
+          for j := 0 to FilesPerStation - 1 do
+          begin
+            Range := WordDocument.Tables.Item(1).Cell((i * (FilesPerStation + 1)) + 2 + j, 1).Range;
+            Range.Select;
+            Shapes := WordApplication.Selection.InlineShapes;
+
+            Shapes.AddPicture(PathToPictures + FilesList[(i * FilesPerStation) + j], false, true, Range);
+            WordApplication.Selection.GoToPrevious(wdGoToGraphic);
+            WordApplication.Selection.Expand(wdCharacter);
+            Shapes.Item(Shapes.Count).LockAspectRatio := msoTrue;
+
+            PictureImage.Picture.LoadFromFile(PathToPictures + FilesList[(i * FilesPerStation) + j]);
+
+            Shapes.Item(Shapes.Count).Width :=
+              Ceil(PixelsToPoints(PictureImage.Picture.Width * seScale.Value / 100));
+            Shapes.Item(Shapes.Count).Height :=
+              Ceil(PixelsToPoints(PictureImage.Picture.Height * seScale.Value / 100));
+
+            Application.ProcessMessages;
+          end;
+        end;
+
+        WordDocument.Tables.Item(1).Rows.Item(1).Select;
+        for i := 1 to StationsList.Count do
+        begin
+          WordApplication.Selection.MoveDown(wdLine, i * (FilesPerStation + 1), wdMove);
+          WordApplication.Selection.InsertBreak(wdPageBreak);
+          Application.ProcessMessages;
+        end;
+
+        WordDocument.SaveAs(edSelectDoc.Text);
+        WordApplication.Visible := true;
+      finally
+        WordDocument.Disconnect;
+      end;
+    except
+      on E: Exception do
+      begin
+        WordApplication.Quit;
+        raise;
+      end;
+    end;
+  finally
+    WordApplication.Disconnect;
+  end;
+end;
+
+procedure TFMain.btnSelectDocClick(Sender: TObject);
+begin
+  SaveDialog.InitialDir := ExtractFilePath(edSelectDoc.Text);
+  if SaveDialog.Execute then
+    edSelectDoc.Text := SaveDialog.FileName;
+end;
+
+procedure TFMain.btnSelectPicturesClick(Sender: TObject);
+var
+  InputDir: string;
+begin
+  InputDir := edSelectPictures.Text;
+  if SelectDirectory('Input dir select', '', InputDir, []) then
+  begin
+    edSelectPictures.Text := InputDir;
+    Counted := false;
+  end;
+end;
+
+procedure TFMain.FormCreate(Sender: TObject);
+begin
+  FilesList := TStringList.Create;
+  StationsList := TStringList.Create;
+  Counted := false;
+end;
+
+procedure TFMain.FormDestroy(Sender: TObject);
+begin
+  FilesList.Free;
+  StationsList.Free;
+end;
+
+function TFMain.GetPathToPictures: string;
+begin
+  Result := IncludeTrailingPathDelimiter(edSelectPictures.Text);
+end;
+
+function TFMain.MillimetersToPoints(aValue: Single): Single;
+begin
+  Result := CentimetersToPoints(aValue / 10);
+end;
+
+function TFMain.PixelsToPoints(aValue: Single): Single;
+begin
+  Result := aValue * 72 / 96;
+end;
+
+procedure TFMain.CountFiles;
+var
+  SR: TSearchRec;
+  Station: string;
+begin
+  FilesList.Clear;
+  StationsList.Clear;
+  if FindFirst(PathToPictures + '*.*', faAnyFile and not faDirectory, SR) = 0 then
+    try
+      repeat
+        Station := ExtractFileName(SR.Name);
+        FilesList.Add(Station);
+        Station := Copy(Station, 1, 4);
+        if StationsList.IndexOf(Station) = -1 then
+          StationsList.Add(Station);
+      until FindNext(SR) <> 0;
+    finally
+      FindClose(SR);
+    end;
+  lblStatus.Caption := Format('%d files for %d stations were found in directory', [FilesList.Count, StationsList.Count]);
+  Counted := true;
+end;
+
+end.
